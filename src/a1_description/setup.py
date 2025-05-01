08787b72a9325974e91d940c80a4bb29dc34a04b
ekaterina <mozhegova04@gmail.com>
2025-05-01 23:47:29 +0300

    updated a1_description

--- src/a1_description/setup.py
+++ src/a1_description/setup.py
@@ -1,4 +1,6 @@
 from setuptools import find_packages, setup
+import os 
+from glob import glob 
 
 package_name = 'a1_description'
 
@@ -10,7 +12,12 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        ('share/' + package_name + '/launch', ['launch/a1_rviz.launch.py']),
+        # Install launch files
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.py')),
+        # Install URDF/Xacro files
+        (os.path.join('share', package_name, 'urdf'), glob('urdf/*.xacro')),
+        # Install Gazebo world files
+        (os.path.join('share', package_name, 'worlds'), glob('worlds/*.world')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
